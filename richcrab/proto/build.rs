255a856b266336feb7ecacade49a101b95aeda50
Alexander <129458005+BeesKnight@users.noreply.github.com>
2026-02-25 23:38:32 +0300

    Add modular protobuf API definitions and build config

--- richcrab/proto/build.rs
+++ richcrab/proto/build.rs
@@ -1,8 +1,23 @@
 fn main() {
-    println!("cargo:rerun-if-changed=proto/richcrab.proto");
+    println!("cargo:rerun-if-changed=proto");
+
+    let protoc = protoc_bin_vendored::protoc_bin_path().expect("failed to find protoc");
+    std::env::set_var("PROTOC", protoc);
+    let protoc_include = protoc_bin_vendored::include_path().expect("failed to find protoc include");
+
+    let protos = [
+        "proto/common.proto",
+        "proto/events.proto",
+        "proto/game.proto",
+        "proto/join.proto",
+        "proto/quiz.proto",
+        "proto/entitlements.proto",
+        "proto/bot.proto",
+    ];
+
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
-        .compile_protos(&["proto/richcrab.proto"], &["proto"])
+        .compile_protos(&protos, &[std::path::Path::new("proto"), protoc_include.as_path()])
         .expect("failed to compile proto definitions");
 }
