9f2835c31c5dd5259ffb638e965bbcdc56917748
Alexander <129458005+BeesKnight@users.noreply.github.com>
2026-02-27 02:43:20 +0300

    fix(frontend): remove useSockets dependency on socketService export

--- frontend/src/hooks/useSockets.ts
+++ frontend/src/hooks/useSockets.ts
@@ -1,6 +1,6 @@
-import { useEffect } from 'react';
-import { socketService } from '../services/socket';
-import { useAppDispatch } from '../store/hooks';
+import { useEffect } from 'react'
+import { connectSocket, disconnectSocket, getSocket } from '../services/socket'
+import { useAppDispatch } from '../store/hooks'
 import {
   setPlayers,
   setTeams,
@@ -9,64 +9,70 @@ import {
   updateScore,
   setStatus,
   setMyTeamTurn,
-} from '../store/slices/gameSlice';
+} from '../store/slices/gameSlice'
 
 export const useGameSocket = (pin: string, playerId: string) => {
-  const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch()
 
   useEffect(() => {
-    if (!socketService.getSocket()) {
-      socketService.connect();
+    if (!getSocket()) {
+      connectSocket('guest', pin)
     }
 
-    const socket = socketService.getSocket()!;
-    socketService.joinGameRoom(pin, playerId);
+    const socket = getSocket()
+    if (!socket) {
+      return
+    }
+
+    socket.emit('join-game', { pin, playerId })
 
     const onPlayersUpdate = (data: { players: any[]; teamA: any[]; teamB: any[] }) => {
-      dispatch(setPlayers(data.players));
-      dispatch(setTeams({ teamA: data.teamA, teamB: data.teamB }));
-    };
+      dispatch(setPlayers(data.players))
+      dispatch(setTeams({ teamA: data.teamA, teamB: data.teamB }))
+    }
 
     const onGameStarted = () => {
-      dispatch(setStatus('playing'));
-    };
+      dispatch(setStatus('playing'))
+    }
 
     const onQuestion = (data: { question: any; timeLimit: number }) => {
-      dispatch(setCurrentQuestion(data.question));
-      dispatch(setTimeLeft(data.timeLimit));
-    };
+      dispatch(setCurrentQuestion(data.question))
+      dispatch(setTimeLeft(data.timeLimit))
+    }
 
     const onTurn = (data: { team: 'A' | 'B' }) => {
-      dispatch(setMyTeamTurn(data.team === 'A'));
-    };
+      dispatch(setMyTeamTurn(data.team === 'A'))
+    }
 
     const onScoreUpdate = (data: { teamA: number; teamB: number }) => {
-    };
+      dispatch(updateScore({ teamA: data.teamA, teamB: data.teamB }))
+    }
 
     const onTimeUpdate = (data: { timeLeft: number }) => {
-      dispatch(setTimeLeft(data.timeLeft));
-    };
+      dispatch(setTimeLeft(data.timeLeft))
+    }
 
-    const onGameFinished = (data: { winner: 'A' | 'B' | 'draw' }) => {
-      dispatch(setStatus('finished'));
-    };
+    const onGameFinished = (_data: { winner: 'A' | 'B' | 'draw' }) => {
+      dispatch(setStatus('finished'))
+    }
 
-    socket.on('players-update', onPlayersUpdate);
-    socket.on('game-started', onGameStarted);
-    socket.on('question', onQuestion);
-    socket.on('turn', onTurn);
-    socket.on('score-update', onScoreUpdate);
-    socket.on('time-update', onTimeUpdate);
-    socket.on('game-finished', onGameFinished);
+    socket.on('players-update', onPlayersUpdate)
+    socket.on('game-started', onGameStarted)
+    socket.on('question', onQuestion)
+    socket.on('turn', onTurn)
+    socket.on('score-update', onScoreUpdate)
+    socket.on('time-update', onTimeUpdate)
+    socket.on('game-finished', onGameFinished)
 
     return () => {
-      socket.off('players-update', onPlayersUpdate);
-      socket.off('game-started', onGameStarted);
-      socket.off('question', onQuestion);
-      socket.off('turn', onTurn);
-      socket.off('score-update', onScoreUpdate);
-      socket.off('time-update', onTimeUpdate);
-      socket.off('game-finished', onGameFinished);
-    };
-  }, [pin, playerId, dispatch]);
-};
+      socket.off('players-update', onPlayersUpdate)
+      socket.off('game-started', onGameStarted)
+      socket.off('question', onQuestion)
+      socket.off('turn', onTurn)
+      socket.off('score-update', onScoreUpdate)
+      socket.off('time-update', onTimeUpdate)
+      socket.off('game-finished', onGameFinished)
+      disconnectSocket()
+    }
+  }, [pin, playerId, dispatch])
+}
